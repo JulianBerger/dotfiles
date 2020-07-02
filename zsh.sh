eeac0750e96700ebdd780a26249fe80ad10a38fd
JulianBerger <jberger95@gmail.com>
2020-07-02 14:56:59 +0200

    vscode as our preferred editor for local files, vim for ssh

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
+# Preferred editor for local and remote sessions
+echo "if [[ -n $SSH_CONNECTION ]]; then # SSH mode" >> ~/.zshrc
+echo "  export EDITOR='vim'" >> ~/.zshrc
+echo "else # Local terminal mode" >> ~/.zshrc
+echo "  export EDITOR='code -w'" >> ~/.zshrc
+echo "fi" >> ~/.zshrc
+
 # Aliases for VSCode to edit zshrc
-echo "alias zshconfig=\"code ~/.zshrc\"" >> ~/.zshrc
-echo "alias ohmyzsh=\"code ~/.oh-my-zsh\"" >> ~/.zshrc
+echo "alias zshconfig='code ~/.zshrc' >> ~/.zshrc
+echo "alias ohmyzsh='code ~/.oh-my-zsh' >> ~/.zshrc
 # Setup zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 # Run neofetch on zsh start
