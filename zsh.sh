a00c4c4711f06fa8a06ce18e3b5f3e72fb592f63
JulianBerger <jberger95@gmail.com>
2020-07-02 14:53:24 +0200

    add aliases for zsh  config edit

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env zsh
 
-
+# Aliases for VSCode to edit zshrc
+echo "alias zshconfig=\"code ~/.zshrc\"" >> ~/.zshrc
+echo "alias ohmyzsh=\"code ~/.oh-my-zsh\"" >> ~/.zshrc
 # Setup zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+# Run neofetch on zsh start
 echo "neofetch" >> ~/.zshrc
 source ~/.zshrc
